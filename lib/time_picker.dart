5685b9a534cc3709fce0bd595b74030a9b0aa676
Legolas1086 <sanjayurs16@gmail.com>
2020-10-21 18:39:05 +0530

    initial setup

---
+++ lib/time_picker.dart
@@ -0,0 +1,20 @@
+
+
+import 'package:flutter/material.dart';
+
+// ignore: camel_case_types
+class time_picker{
+  static TimeOfDay time= TimeOfDay.now();
+  // ignore: non_constant_identifier_names
+  static String time_selected='';
+  // ignore: missing_return
+  static Future<void> selecttime(BuildContext context)async{
+    time=await showTimePicker(
+        context: context,
+        initialTime: TimeOfDay.now()
+    );
+    if(time!=null){
+      time_selected=time.format(context);
+    }
+  }
+}
